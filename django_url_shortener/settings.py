c7f7e2e43b3912aaf64ad8ca2b04975cfbc2c75d
flizz <kakun12311@gmail.com>
2020-05-25 22:00:43 +0200

    Revert "changed some files for heroku"
    This reverts commit 2f23666ffce1493e04d9050386a22f37f36e69a9.

--- django_url_shortener/settings.py
+++ django_url_shortener/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v8vgj6lx_-axy=3chj+#)s8l7ud^ti_m#x0q$8dfvw4hm+6wsf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -77,12 +76,8 @@ WSGI_APPLICATION = 'django_url_shortener.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django-url-shortener',
-        'USER': 'admin',
-        'PASSWORD': 'KursUnity234!',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -125,7 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-BASE_URL = 'https://django-link-shortener.herokuapp.com/'
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+BASE_URL = 'http://127.0.0.1:8000/'
